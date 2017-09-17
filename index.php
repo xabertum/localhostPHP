0e9efdff78ed13899a81326b9f136fb77d209333
xabertum <mohatar@gmail.com>
2017-09-17 14:07:33 +0200

    Added File

--- index.php
+++ index.php
@@ -64,7 +64,9 @@ and open the template in the editor.
             return $z;
         }
         
-        echo "5 + 10 = " . sum(5, 10) . ""
+        echo "5 + 10 = " . sum(5, 10) . "<br>";
+        echo "7 + 13 = " . sum(7,13) . "<br>";
+        echo "2 + 4 = " . sum(2,4);
         
         ?>
     </body>
